cb33795ba3d07e3c68593bd9d866c45c9196b334
Brendan Dutertre <brendan.dutertre@gmail.com>
2021-11-25 16:26:27 +0100

    pom

--- notolib/src/main/java/org/miage/camel/gateways/CallForFundsImpl.java
+++ notolib/src/main/java/org/miage/camel/gateways/CallForFundsImpl.java
@@ -1,8 +1,8 @@
 package org.miage.camel.gateways;
 
+import dto.CallForFunds;
 import org.apache.camel.CamelContext;
 import org.apache.camel.ProducerTemplate;
-import org.miage.dto.CallForFunds;
 
 import javax.inject.Inject;
 import java.io.IOException;
@@ -15,7 +15,7 @@ public class CallForFundsImpl implements CallForFundsGateway{
     @Override
     public void sendCallForFunds(float amount, String rib, String reason) {
         try (ProducerTemplate producer = context.createProducerTemplate()) {
-            producer.sendBody("direct:CFD", new CallForFunds(amount, rib, reason));
+           producer.sendBody("direct:CFD", new CallForFunds(amount, rib, reason));
         } catch (IOException e) {
             e.printStackTrace();
         }
