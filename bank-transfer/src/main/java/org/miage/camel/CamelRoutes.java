902b0b19b314f7adcf3a5287d30962548043ccc7
Brendan Dutertre <brendan.dutertre@gmail.com>
2021-12-02 12:32:12 +0100

    camel route

--- bank-transfer/src/main/java/org/miage/camel/CamelRoutes.java
+++ bank-transfer/src/main/java/org/miage/camel/CamelRoutes.java
@@ -1,6 +1,7 @@
 package org.miage.camel;
 
 import dto.CallForFunds;
+import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
 import org.miage.service.TransferService;
@@ -23,12 +24,15 @@ public class CamelRoutes extends RouteBuilder {
     @Inject
     BeanTest2 beanTest2;
 
+    @Inject
+    CamelContext camelContext;
+
     @Override
     public void configure() throws Exception {
 
+        //camelContext.setTracing(true);
 
-        from("jms:queue:BKRS:" + suffix + ":CFF")
-                .log("${body} - ${header.bankCreditorRoute}")
+        from("jms:queue:BKRS/" + suffix + "/CFF")
                 .unmarshal().json(CallForFunds.class)
                 //.bean(loanService)//TODO Creer un prêt
                 .bean(transferService) //TODO Creer et return un virement
@@ -43,10 +47,10 @@ public class CamelRoutes extends RouteBuilder {
         .bean(virementService); ////TODO Récupère un virment et l'ajoute, use ?value =   ou un filter piper*/
 
         //ajouter aussi dans l'autre bank
-        from("jms:queue:BKRS:" + suffix + ":RIB")
+        from("jms:queue:BKRS/" + suffix + "/RIB")
                 .bean(beanTest); //TODO retourner un rib par l'email
 
-        from("jms:queue:BKRS:FR129:RIB")
+        from("jms:queue:BKRS/FR129/RIB")
                 .bean(beanTest2); //TODO retourner un rib par l'email
 
     }
--- fund_system/src/main/java/org/miage/camel/CamelRoutes.java
+++ fund_system/src/main/java/org/miage/camel/CamelRoutes.java
@@ -25,21 +25,22 @@ public class CamelRoutes extends RouteBuilder {
 
         //camelContext.setTracing(true);
 
-        from("jms:queue:BKRS:booking")
+        from("jms:queue:BKRS/booking")
                 .log("${body}")
                 .split(jsonpath("$.*"), callForFoundAggregationStrategy).streaming()    //For each line of json
                 .choice().when(body().contains("@"))    //If it's an email --> get RIB
                 .multicast(ribAggregationStrategy)
-                .to("jms:queue:BKRS:FR121:RIB?exchangePattern=InOut")
-                .to("jms:queue:BKRS:FR129:RIB?exchangePattern=InOut")
+                .to("jms:queue:BKRS/FR121/RIB?exchangePattern=InOut")
+                .to("jms:queue:BKRS/FR129/RIB?exchangePattern=InOut")
                 .end()
                 .log("RIB found : ${body}")
                 .endChoice()
                 .end().end()
                 .log("CFF : ${body}")
                 .marshal().json()
-                .log("CFF : ${header.bankAcquirerRoute}");
-                //.toD("jms:queue:BKRS:${header.bankAcquirerRoute}:CFF");
+                .log("CFF : ${header.bankAcquirerRoute}")
+                .toD("jms:queue:BKRS/${header.bankAcquirerRoute}/CFF")
+                .end();
 
     }
 }
